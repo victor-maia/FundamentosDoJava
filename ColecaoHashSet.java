f0eb9f3d594ac58dd437950fe691faaab26e52df
Victor Maia <victorluiscm@gmail.com>
2024-06-05 16:24:04 -0300

    Trabalhando com conceitos da coleção hashset

---
+++ ColecaoHashSet.java
@@ -0,0 +1,24 @@
+package Fundamentos;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class ColecaoHashSet {
+    public static void main(String[] args) {
+        Set<Integer> numeros = new HashSet<>();
+        numeros.add(10);
+        numeros.add(11);
+        numeros.add(12);  
+
+
+        numeros.remove(12);
+
+
+        for(Integer elemento : numeros){
+           System.out.println(elemento);
+       }
+
+
+        System.out.println(numeros.contains(12));
+    }
+}
